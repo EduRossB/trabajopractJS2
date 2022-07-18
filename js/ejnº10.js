5f093c36686de60d9fec56e74489bec2694585cc
EduRossB <edu_barba@outlook.com>
2022-07-17 21:20:36 -0300

    ejercicio 10 terminado

--- js/ejnº10.js
+++ js/ejnº10.js
@@ -1,14 +1,16 @@
 // Pedir numero de filas y columnas   X
-// dibujar una tabla    
+// dibujar una tabla        X
 
 // Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la 
 // tabla es de 7×5 los números irán del 35 al 1.
 
-let filas = parseInt(prompt("Ingrese un numero de filas"))
-let columnas = parseInt(prompt("Ingrese un numero de columnas"))
+const filas = parseInt(prompt("Ingrese un numero de filas"))
+const columnas = parseInt(prompt("Ingrese un numero de columnas"))
+let resultado = filas * columnas
 
 console.log(filas)
 console.log(columnas)
+console.log(resultado)
 
 document.write(`<table>
 <thead>`)
@@ -17,7 +19,8 @@ for(let indicefila = 1; indicefila <= filas; indicefila++){
 
     document.write(`<tr>`)
     for(let indicecolumna = 1; indicecolumna <= columnas; indicecolumna++){
-    document.write(`<td>Hola</td>`)
+    document.write(`<td>${resultado}</td>`)
+    resultado--
     }
 
     document.write(`</tr>`)
