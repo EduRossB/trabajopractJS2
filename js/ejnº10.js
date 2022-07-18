3c43e3afc2e90f926c8fbce10eb0561924ee8f0d
EduRossB <edu_barba@outlook.com>
2022-07-17 21:13:53 -0300

    programa dibuja columnas

--- js/ejnº10.js
+++ js/ejnº10.js
@@ -5,22 +5,22 @@
 // tabla es de 7×5 los números irán del 35 al 1.
 
 let filas = parseInt(prompt("Ingrese un numero de filas"))
-// let columnas = parseInt(prompt("Ingrese un numero de columnas"))
+let columnas = parseInt(prompt("Ingrese un numero de columnas"))
 
-// console.log(filas)
-// console.log(columnas)
+console.log(filas)
+console.log(columnas)
 
 document.write(`<table>
 <thead>`)
 
 for(let indicefila = 1; indicefila <= filas; indicefila++){
 
-    document.write(`<tr>
-<td>Hola</td>
-<td>mundo</td>
-</tr>`)
-    
+    document.write(`<tr>`)
+    for(let indicecolumna = 1; indicecolumna <= columnas; indicecolumna++){
+    document.write(`<td>Hola</td>`)
+    }
 
+    document.write(`</tr>`)
 }
 
 
